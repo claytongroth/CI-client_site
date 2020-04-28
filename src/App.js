8d80ae2d94d502e92fb2b8fef431073734c78bf5
claytongroth <claytoncgrth@gmail.com>
2020-04-28 17:00:19 -0600

    Initial content commit.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Your stuff will go here.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
