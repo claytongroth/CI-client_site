358c1665d9810c94ce858014e5c0fd2ada366651
claytongroth <claytoncgrth@gmail.com>
2020-04-28 17:16:17 -0600

    Test CI

---
+++ build/precache-manifest.04e69f69982728cce26813dc6b81e1d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006564be055bcfd734af9c22b1219357",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42a196eb0427c9b2b153",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5084c592590de9a5f2a8",
+    "url": "/static/js/2.3cd48cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3cd48cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a196eb0427c9b2b153",
+    "url": "/static/js/main.eebb491e.chunk.js"
+  },
+  {
+    "revision": "89c3193a79ed8e5df358",
+    "url": "/static/js/runtime-main.ee49d144.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Your stuff will go here.
+          Your stuff will go here. (Update)
         </p>
       </header>
     </div>
